3d782428386c847cf7c7bc4f011fdce6a6f8901f
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-09-28 10:10:14 -0400

    Creating PlaceBet Method

--- Roulette Game/Program.cs
+++ Roulette Game/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace Roulette_Game
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static int creditsTotal = 1000;
+
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Roulette table, you currently have 1000 credits");
+            PlaceBet();
+        }
+        
+        public static void PlaceBet()
+        {
+            Console.WriteLine("What is your bet amount?");
+            int betAmount = Convert.ToInt32(Console.ReadLine());
+
+            if (betAmount > creditsTotal || betAmount <= 0)
+                Console.WriteLine("Insufficient Funds");
+            else if (betAmount <= creditsTotal)
+                Console.WriteLine("your current bet is {0}", betAmount);
         }
     }
 }
